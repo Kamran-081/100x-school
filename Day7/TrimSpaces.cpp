a3c09c5afd03ee0226215b3ef45b6c57445db7b8
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-06 23:54:14 +0530

    Create TrimSpaces.cpp

---
+++ Day7/TrimSpaces.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=' ') cout<<s[i];
+    }
+}
