5dda2c6daeee671b0de244bcafb2138f6d5a1a30
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-06 23:51:48 +0530

    Add Brothers.cpp to compare last names

---
+++ Day7/Brothers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string f1,l1,f2,l2;
+    cin>>f1>>l1;
+    cin>>f2>>l2;
+
+    if(l1==l2) cout<<"YES";
+    else cout<<"NO";
+}
