505b2b3f305e04e50ce3502e4ed9da7e34486e52
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-11 22:22:19 +0530

    Create CountPrimes.cpp

---
+++ Day10/CountPrimes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool prime[100001];
+
+    for(int i = 0; i <= n; i++)
+        prime[i] = true;
+
+    prime[0] = prime[1] = false;
+
+    for(int i = 2; i * i <= n; i++) {
+        if(prime[i]) {
+            for(int j = i * i; j <= n; j += i)
+                prime[j] = false;
+        }
+    }
+
+    int count = 0;
+
+    for(int i = 2; i <= n; i++)
+        if(prime[i])
+            count++;
+
+    cout << count;
+
+    return 0;
+}
